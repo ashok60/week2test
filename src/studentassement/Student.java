bed009b808b95c20613d637b2af6b30471b2f4f0
ashok <ashokcherry60@gmail.com>
2020-08-07 16:22:11 +0530

    first commit

--- src/studentassement/Student.java
+++ src/studentassement/Student.java
@@ -1,18 +1,54 @@
 package studentassement;
 import java.util.*;
 public class Student {	
-	private String rollno;	
-	private int age;	
-	public String getRoll() {
-		return rollno;
-	}
-	public void setRoll(String roll) {
-		this.rollno = roll;
-	}
+	
+		private String rollno;
+		private int age; 
+		
+		public Student(String rollno,int age){
+			this.rollno=rollno;
+			this.age=age;
+			
+		}
+		
+		public String getRollNo() {
+			return rollno;
+		}
+
+	public void setRollNo(String rollno) {
+			this.rollno = rollno;
+		}
+
 	public int getAge() {
-		return age;
+			return age;
+		}
+		
+		public void setAge(int age) {
+			this.age=age;
+		}	
+		@Override
+		public int hashCode() {
+			int hashcode=rollno.hashCode();
+			return hashcode;
+		}
+		@Override
+		public boolean equals(Object arg) {
+			if(this==arg) {
+				return true;
+			}
+			
+		   if(arg==null || !(arg instanceof Student) ) {
+				return false;
+		   }
+			
+		    Student that=(Student)arg;	
+			boolean isequal=this.rollno .equals(that.rollno);
+			return isequal;
+		}
+		
+		
+		
+			
+		
+			
 	}
-	public void setAge(int age) {
-		this.age = age;
-	}
-}
--- src/studentassement/StudentMain.java
+++ src/studentassement/StudentMain.java
@@ -1,40 +1,29 @@
 package studentassement;
 import java.util.*;
-public class StudentMain{
-void validate (int age) throws AgeException {
-	if (age<21){
-		throws new AgeException();
+
+public class StudentMain {
+	private Set<Student> set = new HashSet<>();
+
+	public static void main(String[] args) {
+		StudentMain run = new StudentMain();
+		run.runApp();
 	}
-}
 
+	public void runApp() {
+		set.add(new Student("a1", 23));
+		set.add(new Student("b2", 21));
+		set.add(new Student("c3", 20));
+		set.add(new Student("d4", 22));
+		List<Student> list = changedlist(set);
+	}
 
-public static void main(String [] args) {
-	
-	Set<Student> hset = new HashSet<>();
-	Set<Student> hset1 = new HashSet<>();
-	Student s1 = new Student("101",20);
-	Student s2 = new Student("102,21);
-	Student s3 = new Student("103",24);
-	hset.add(s1);
-	hset.add(s2);
-	hset.add(s3);
-	
-	System.out.println("Students List");
-	for(Student s:hset){
-		try {
-			System.out.println("Rollno "+s.getRollno()+" Age: "+s.getAge());
-			StudentMain ob = new StudentMain();
-			ob.validate(age);
-			hset1.add(s);
+	public List<Student> changedlist(Set<Student> set) {
+		List<Student> list = new ArrayList<>();
+		for (Student student : set) {
+			if (student.getAge() > 21) {
+				list.add(student);
+			}
 		}
-		catch (AgeException e){
-			System.out.println(e);
-		}		
-			
+		return list;
 	}
-	System.out.println("Students with Age greater than or equal to 21 ");
-	for(Student s:hset1){
-		System.out.println(" Rollno "+s.getrollno()+" Age: "+s.getAge());
-	}
-	
 }
